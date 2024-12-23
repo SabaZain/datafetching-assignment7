e8eab7310e4d2a2803a573293e173eed8beaac52
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-12-24 02:46:40 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const HomePage = () => {
       
     <div className='flex flex-col items-center justify-center h-screen bg-slate-400 border-black'>
       <div className='bg-black p-12 border-neutral-900 rounded-xl'>
-      <h1 className='text-xl md:text-2xl lg:text-2xl text-center text-white font-serif font-bold p-4'>Data Fetching Application <br/> By Saba Zain</h1>
+      <h1 className='text-2xl text-center text-white font-serif font-bold p-4'>Data Fetching Application <br/> By Saba Zain</h1>
       <div className='grid gap-4'>
       <button className='bg-purple-600 hover:bg-red-400 hover:text-white border-black rounded-md p-2 text-[18px] font-serif font-bold'><Link href="Client-Side">Client Side Data Fetching </Link></button>
       <button className='bg-purple-600 hover:bg-red-400 hover:text-white border-black rounded-md p-2 text-[18px] font-serif font-bold'><Link href="Server-Side">Server Side Data Fetching</Link></button>
