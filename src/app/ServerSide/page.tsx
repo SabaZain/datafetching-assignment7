cbb5c3b1084120a7656892887ddf2502eeac8075
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-12-24 02:55:00 +0500

    Update page.tsx

--- src/app/ServerSide/page.tsx
+++ src/app/ServerSide/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ServerSideRendering = () => {
   return (
-    <div className='mb-44 md:mb-64 lg:mb-72'>
+    <div className='mb-44 md:mb-64 lg:mb-72 mx-3'>
       <h1 className='text-2xl text-center font-bold mt-10'>Server Side Rendering</h1>
       <p className='text-center mt-6'>Server Side Rendering(SSR) is a web development technique where the content of a webpage is generated on the server and then sent
       as fully rendered HTML to the client(browser).In SSR, the server processes the requested page, renders the necessary content, 
