9edec47d11e6dfca016fa9939033dd0b0907a2e4
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-12-24 02:30:23 +0500

    Update page.tsx

--- src/app/Server-Side/page.tsx
+++ src/app/Server-Side/page.tsx
@@ -8,26 +8,17 @@ interface Book {
 
     async function fetchBooks(): Promise<Book[]>{
     const res = await fetch("https://simple-books-api.glitch.me/books/");
-    
-    if(!res.ok){
-        throw new Error("Failed to fetch books");
-        }
-
-        const data:Book[] = await res.json();
-        return data;
+    const data:Book[] = await res.json();
+    return data;
     }
 
     const Page = async() => {
-        try{
-            const books = await fetchBooks();
+    const books = await fetchBooks();
     return (
     <div>
       <h1 className='text-center text-3xl font-bold mb-8 mt-8'>Server Side Data Fetching</h1>
       <div>
-        {books.length === 0 ? (
-            <p>Books Not Found.</p>
-        ) : (
-            books.map((book) => (
+          {books.map((book) => (
                 
             <div key={book.id} className=' grid p-7 m-6 mx-12 sm:mx-12 md:mx-40 lg:mx-80 bg-slate-300 hover:bg-purple-300 border-violet-900 shadow-inner border rounded-lg '>
                 <h1 className='text-[16px] text-center font-bold'>Id: {book.id}</h1>
@@ -35,16 +26,12 @@ interface Book {
                 <p className='text-center text-green-950 font-medium'>Type: {book.type}</p>
                 <button className='bg-black hover:bg-white text-white hover:text-black hover:font-bold p-1 mx-28 sm:mx-28 md:mx-40 lg:mx-56 mt-3 rounded-md'>See More</button>
             </div>
-            
             ))
-        )}
+        }
       </div>
     </div>
   );
-} catch (error) {
-    return <div>Error Loading Books</div>;
-  }
-}
+};
 
 
 export default Page;
