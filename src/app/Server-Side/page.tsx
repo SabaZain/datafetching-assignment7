28220c721f70154cf5b500ad360d1201240a212d
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-12-25 02:11:58 +0500

    Update page.tsx

--- src/app/Server-Side/page.tsx
+++ src/app/Server-Side/page.tsx
@@ -24,8 +24,10 @@ interface Book {
                 <h1 className='text-[16px] text-center font-bold'>Id: {book.id}</h1>
                 <h2 className='text-xl text-center text-blue-950 font-semibold'>Name: {book.name}</h2>
                 <p className='text-center text-green-950 font-medium'>Type: {book.type}</p>
-                <button className='bg-black hover:bg-white text-white hover:text-black hover:font-bold p-1 mx-28 sm:mx-28 md:mx-40 lg:mx-56 mt-3 mb-3 rounded-md'>See More</button>
-            </div>
+                <div>
+                <button className='bg-black hover:bg-white text-white hover:text-black hover:font-bold p-2 mt-3 mb-3 rounded-md'>See More</button>
+                </div>
+                </div>
             ))
         }
       </div>
