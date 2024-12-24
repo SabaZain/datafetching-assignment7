99a6109df363409bdd28b09028f865fdda88a154
Saba Zain <159971290+SabaZain@users.noreply.github.com>
2024-12-25 01:57:51 +0500

    Update page.tsx

--- src/app/Server-Side/page.tsx
+++ src/app/Server-Side/page.tsx
@@ -20,11 +20,11 @@ interface Book {
       <div>
           {books.map((book) => (
                 
-            <div key={book.id} className=' grid p-7 m-6 mx-12 sm:mx-12 md:mx-40 lg:mx-80 bg-slate-300 hover:bg-purple-300 border-violet-900 shadow-inner border rounded-lg '>
+            <div key={book.id} className=' grid m-6 mx-12 sm:mx-12 md:mx-40 lg:mx-80 bg-slate-300 hover:bg-purple-300 border-violet-900 shadow-inner border rounded-lg '>
                 <h1 className='text-[16px] text-center font-bold'>Id: {book.id}</h1>
                 <h2 className='text-xl text-center text-blue-950 font-semibold'>Name: {book.name}</h2>
                 <p className='text-center text-green-950 font-medium'>Type: {book.type}</p>
-                <button className='bg-black hover:bg-white text-white hover:text-black hover:font-bold p-1 mx-28 sm:mx-28 md:mx-40 lg:mx-56 mt-3 rounded-md'>See More</button>
+                <button className='bg-black hover:bg-white text-white hover:text-black hover:font-bold p-1 mx-28 sm:mx-28 md:mx-40 lg:mx-56 mt-3 mb-3 rounded-md'>See More</button>
             </div>
             ))
         }
